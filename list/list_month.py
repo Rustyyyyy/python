680c5359ebf9619fa431cdfed3c03fd9f03d388c
Rustyyyyy <maharjanajul18@gmail.com>
2020-04-01 08:58:41 +0545

    initial

---
+++ list/list_month.py
@@ -0,0 +1,13 @@
+print("List of month in a year")
+
+month = ("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
+
+
+for eachMonth in month:
+    print(eachMonth)
+
+first_half = month[:6]
+print(first_half)
+
+second_half = month[6:]
+print(second_half)
---
+++ list/list_week.py
@@ -0,0 +1,1 @@
+print("list of days in a week")
