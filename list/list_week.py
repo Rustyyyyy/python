d5e169122796298c7ef34e77febd82b07af30128
Rustyyyyy <maharjanajul18@gmail.com>
2020-04-01 09:06:41 +0545

    initial

--- list/list_week.py
+++ list/list_week.py
@@ -1,1 +1,15 @@
 print("list of days in a week")
+
+day = ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+
+for everyDay in day:
+    print(everyDay)
+
+first_half = day[:3]
+print(first_half)
+
+second_half = day[3:6]
+print(second_half)
+
+last_day = day[-1]
+print(last_day)
